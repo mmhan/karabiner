447056bd4e2ff20ad81bce9a80a1da0d6b3cdc0e
Mike Myat Min Han <mmhan@indeed.com>
2026-06-30 11:14:27 +0100

    Revert terminal to iTerm2

--- rules.ts
+++ rules.ts
@@ -72,7 +72,7 @@ const rules: KarabinerRules[] = [
     o: {
       a: app("Arc"),
       s: app("Slack"),
-      t: app("WezTerm"),
+      t: app("iTerm2"),
       z: app("zoom.us"),
       // "M"arkdown (Reflect.app)
       // m: app("Reflect"),
